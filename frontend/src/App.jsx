59c77a5635d169d84582b3a74119911feae30724
Stevan Stevanovic <stevans93@gmail.com>
2023-11-08 15:11:56 +0100

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-center'>
+      Hello
+      </div>
+    </>
+  )
+}
+
+export default App
