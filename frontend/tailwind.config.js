eb357bda955873fc7602032d22bf4410421f95f0
aleksa212 <90782151+aleksa212@users.noreply.github.com>
2023-12-04 22:14:34 +0100

    add user form

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,26 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       transitionProperty: {
-        'height': 'height',
+        height: "height",
       },
     },
     colors: {
-      main: '#A3C8DB',
-      secondMain: '#5B8FA9',
-      primary: '#A3C8DB',
-      secondary: '#1B1F23',
-      red: 'rgb(220 38 38)',
-      grey: '#A8A8A8',
-      lightGray: '#F8F8F8',
-      spanGray: '#9E9E9E',
-    }
+      main: "#A3C8DB",
+      secondMain: "#5B8FA9",
+      primary: "#A3C8DB",
+      secondary: "#1B1F23",
+      red: "rgb(220 38 38)",
+      grey: "#A8A8A8",
+      lightGray: "#F8F8F8",
+      spanGray: "#9E9E9E",
+      white: "#FFFFFF",
+    },
   },
   plugins: [],
-}
-
+};
