d4499a130b05bebb184815aedfdfab2a73f4e427
emirlemes <emir.lemes@gmail.com>
2023-12-04 22:23:18 +0100

    test

--- frontend/src/components/MapSideBar/MapSideBar.jsx
+++ frontend/src/components/MapSideBar/MapSideBar.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
+import React from 'react'
 
-function MapSideBar({ handleToggleInfo, selectedTitle, selectedMunicipality }) {
-  console.log('MapSide: ', selectedMunicipality);
+function MapSideBar({handleToggleInfo, selectedTitle, selectedMunicipality}) {
+  // console.log('MapSide: ', selectedMunicipality);
 
   return (
     <div className={`w-full h-full bg-[#fff] rounded-2xl shadow relative p-3`}>
       {selectedMunicipality && (
         <div>
-          <p>Okrug: {selectedTitle}</p> <br />
+          {/* <p>Okrug: {selectedTitle}</p> <br /> */}
           <p onClick={handleToggleInfo}>
             {selectedMunicipality
-              .filter(item => item.district === selectedTitle)
+              .filter((item) => item.district === selectedTitle)
               .map((item) => (
-                <span key={item._id}>
-                  <p>Opstine: {item.municipality}</p>
-                  <p>Grad: {item.city}</p> <br />
+                <span key={item._id} className="flex p-3 ">
+                  <button className=" border border-1 border-main px-5 py-2 rounded-xl text-main hover:bg-main hover:text-[#fff]">
+                    {item.municipality}
+                  </button>
                 </span>
               ))}
           </p>
         </div>
       )}
     </div>
-  );
+  )
 }
 
-export default MapSideBar;
+export default MapSideBar
